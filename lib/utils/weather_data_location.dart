dfd8cd4b6150b5d7acaf390e59cfd1204f2a1d2e
Jatin <chaudharyjatin115@protonmail.com>
2021-09-19 17:11:08 +0530

    adding some improvements on weather icon

--- lib/utils/weather_data_location.dart
+++ lib/utils/weather_data_location.dart
@@ -23,23 +23,35 @@ class WeatherData extends ChangeNotifier {
   }
 
 //returns the cupertino weather icon based on condition
-  IconData getWeatherIcon(
+IconData getWeatherIcon(
     int condition,
   ) {
-    if (condition < 232) {
+    if (condition < 233) {
       return CupertinoIcons.cloud_bolt_rain;
     } else if (condition == 800) {
       return CupertinoIcons.sun_max;
-    } else if (condition <= 804) {
-      return CupertinoIcons.cloud_heavyrain;
+    } else if (condition > 801) {
+      return CupertinoIcons.cloud;
     } else if (condition < 700) {
       return CupertinoIcons.cloud_snow_fill;
-    } else if (condition >= 600) {
+    } else if (condition < 623) {
       return CupertinoIcons.snow;
-    } else if (condition >= 300) {
+    } else if (condition < 322) {
       return CupertinoIcons.cloud_drizzle;
+    } else if (condition < 532) {
+      return CupertinoIcons.cloud_rain;
+    } else if (condition == 711) {
+      return CupertinoIcons.smoke;
+    } else if (condition == 721) {
+      return CupertinoIcons.sun_haze;
+    } else if (condition == 741) {
+      return CupertinoIcons.cloud_fog;
+    } else if (condition == 761) {
+      return CupertinoIcons.sun_dust;
+    } else if (condition == 781) {
+      return CupertinoIcons.tornado;
     } else {
-      return CupertinoIcons.cloud;
+      return CupertinoIcons.sun_min;
     }
   }
 
