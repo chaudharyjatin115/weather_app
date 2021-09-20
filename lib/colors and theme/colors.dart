3efecda72a73f97d44d14dbc3b8f30a44947ff55
Jatin Chaudhary <chaudhryjatin115@protonmail.com>
2021-09-20 21:08:58 +0530

    adding more Colors

--- lib/colors and theme/colors.dart
+++ lib/colors and theme/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 //colors and gradients
 Color ksunnygradientColorTop = Color(0xffFFC210);
@@ -7,7 +8,16 @@ Color kRainyGradientColorTop = Color(0xff5BDEFD);
 Color kRainyGradientColorBottom = Color(0xff5acbf9);
 Color kCloudyGradientColorTop = Color(0xff6EFAE6);
 Color kCloudyGradientColorBottom = Color(0xff6DF1E7);
+Color ksnowyColorTop = Colors.blue.shade50;
+Color ksnowyColorBottom = Colors.lightBlue.shade100;
+Color kClearSkytop = Color(0xffA0C4FF);
+Color kClearSkyBottom = Colors.blueAccent;
 Color kTextcolr = Color(0xff302E63);
-
+Color ksandColorTop = Color(0xffB87A40);
+Color ksandColorBottom = Color(0xffDAA55F);
+Color kStormColorTop = Color(0xff494044);
+Color kStormColorBottom = Color(0xffB2937D);
+Color kFogColorTop = Color(0xffFFA8A7);
+Color kFogColorBottom = Colors.white;
 Color kNewColor = Color(0xffF8EDD4);
 Color knewto = Color(0xff9AB9BF);
