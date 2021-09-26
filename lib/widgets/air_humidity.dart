7331c962bf68194911f360097efdb233343ad73a
Jatin Chaudhary <chaudhryjatin115@protonmail.com>
2021-09-26 21:09:55 +0530

    improving wind speed and humidity icons and text

--- lib/widgets/air_humidity.dart
+++ lib/widgets/air_humidity.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:weather_icons/weather_icons.dart';
+
 class AirHumidity extends StatelessWidget {
   final int? humid;
   final double? speed;
@@ -14,29 +16,44 @@ class AirHumidity extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(
-            Icons.air,
-            size: 20.0,
+          Row(
+            children: [
+              Icon(
+                WeatherIcons.wind,
+                size: 15.0,
+              ),
+              SizedBox(width: 20.0),
+              Padding(
+                padding: const EdgeInsets.only(top: 9.0),
+                child: Text(
+                  '$speed km/h',
+                  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+            ],
           ),
-          SizedBox(width: 15.0),
-          Text(
-            '$speed km/h',
-            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
-          ),
-          SizedBox(
-            width: 20.0,
-          ),
-          Icon(
-            CupertinoIcons.drop,
-            size: 20.0,
-          ),
-          SizedBox(
-            width: 20.0,
-          ),
-          Text(
-            '$humid %',
-            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                WeatherIcons.humidity,
+                size: 15.0,
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 9.0),
+                child: Text(
+                  '$humid %',
+                  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
