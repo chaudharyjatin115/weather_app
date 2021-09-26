d9ea6ae4ec7f6bb9eec52442833afeb77ba4f6dc
Jatin Chaudhary <chaudhryjatin115@protonmail.com>
2021-09-26 16:12:37 +0530

    implementing a loading screen

---
+++ lib/Screens.dart/loading.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/Screens.dart/weatherScreen.dart';
+import 'package:weather_app/utils/weather_data_location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherData().getLocationWeather();
+
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => WeatherScreen(weatherData)),
+        (Route<dynamic> route) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            child: CircularProgressIndicator(
+              color: Colors.amber,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
